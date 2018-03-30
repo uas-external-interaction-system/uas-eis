ce54e7ce6c183336c14967dd5fdf8ae7e4c85bca
zhuth <zhuth@usoftchina.com>
2018-03-30 13:52:53 +0800

    初始化项目构建

---
+++ src/main/java/com/uas/eis/UasEisApplication.java
@@ -0,0 +1,18 @@
+package com.uas.eis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class UasEisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UasEisApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello world.";
+	}
+}
